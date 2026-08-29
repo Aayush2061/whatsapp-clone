7ed9cfec5307b873af7a8c13967ef9a630b383c1
Aayush2061 <aayushbhandari6789@gmail.com>
2026-08-29 11:27:03 +0545

    Use environment variable for API URL instead of hardcoded localhost

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -23,7 +23,7 @@ export const SocketProvider = ({children}) => {
             return;
         }
 
-        const newSocket = io('http://localhost:5000', {
+        const newSocket = io(import.meta.env.VITE_API_URL,  {
             query: { userId: user?.id},
         })
     
